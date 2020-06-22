4deb5c0186e4bec7ee6d72abe0a6b42f46976f58
Kyle Williams <kwilliams@peakactivity.com>
2020-06-22 18:47:21 -0400

    added some extra stuff?

--- server.js
+++ server.js
@@ -1,26 +1,15 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const c2j = require('csvtojson')
-const inventory = './ecomm-inventory.csv'
-const sales = './ecomm-sales.csv'
-
-// const request = (file) => {
-//     c2j().fromFile(file)
-//     .then((jsonObj)=>{
-//         console.log(jsonObj) a c
-//         res.send(jsonObj)
-//     })
-//     .catch((err) => {
-//         console.log(err)
-//         res.send('WE MESSED UP')
-//     })
-// }
+// ====== We used this to convert our csv file to json ======
+// const c2j = require('csvtojson')
+// const inventory = './ecomm-inventory.csv'
+// const sales = './ecomm-sales.csv'
 
 const corsOptions = {
     origin: 'http://localhost:3000',
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-  }
+}
 
 app.use(cors(corsOptions))
 
@@ -34,18 +23,7 @@ app.get('/inventory', cors(corsOptions), async (req, res) => {
 
             db.collection('test').find().limit(250).toArray(function(err, items) {
                 if(err) throw err;
-
-                // const toBeReturned = items.reduce((prev, cur) => {
-                //     // console.log('PREV', prev)
-                //     // console.log('CUR', cur)
-                //     return { 
-                //         ...prev, 
-                //         // [cur.Country] just says set cur.Country as the key
-                //         // (prev[cur.Country] || 0) means if the key already existed, else 0
-                //         [cur.Country]: [ ...(prev[cur.Country] || []), cur.UnitPrice ]
-                //     }
-                // }, {})  
-                // return res.send(toBeReturned) 
+                
                 return res.send(items)    
             });          
         });
