2aa06f88060f7ea9d7a2cebefba9c660723b668b
Kyle Williams <kwilliams@peakactivity.com>
2020-06-25 07:47:37 -0400

    06/22/2020

--- server.js
+++ server.js
@@ -1,17 +1,58 @@
 const express = require('express')
 const app = express()
+const cartRouter = require('./cart')
+const bodyParser = require('body-parser')
 const cors = require('cors')
 // ====== We used this to convert our csv file to json ======
 // const c2j = require('csvtojson')
 // const inventory = './ecomm-inventory.csv'
 // const sales = './ecomm-sales.csv'
 
+// ----------- AUX VALUES ------------------
 const corsOptions = {
     origin: 'http://localhost:3000',
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 
+
+// STATUS CODES (apple example)
+
+// ----------- MIDDLEWARE ------------------
+
+app.use('/api/cart', cartRouter)
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json
+app.use(bodyParser.json())
+
+
+// kyle youre not being smart here !!!
 app.use(cors(corsOptions))
+
+// const cors2 = (corsOptions) => (req, res, next) => {
+//     //handle differently based on cors options
+//     return res.status(corsOptions.optionsSuccessStatus || 200).send(...)
+// }
+
+// // !!!!!!!!! THIS IS WHAT WE WILL USE FOR AUTHENTICATION !!!!!!!
+// app.use((req, res, next) => {
+
+// })
+
+// ----------- ROUTES ------------------
+
+app.post('/blah', (req, res) => {
+    // console.log('THE BODY IS ===> ' + JSON.stringify(req.body) + 
+    // '\nTHE BODY MESSAGE IS ===> ' + req.body.message +
+    // '\nTHE BODY HEADERS ARE ===> ' + JSON.stringify(req.headers))
+    return res.status(201).send(
+        'THE BODY IS ===> ' + JSON.stringify(req.body) + 
+        '\nTHE BODY MESSAGE IS ===> ' + req.body.message +
+        '\nTHE BODY HEADERS ARE ===> ' + JSON.stringify(req.headers)
+    )
+})
 
 app.get('/inventory', cors(corsOptions), async (req, res) => {
     const MongoClient = require('mongodb').MongoClient;
@@ -36,7 +77,7 @@ app.get('/piechart', async (req, res) => {
     this.request(sale)
 })
 
-
+// ----------- LISTEN TO CALLS ------------------
 
 app.listen(
     process.env.PORT || 7744, 
