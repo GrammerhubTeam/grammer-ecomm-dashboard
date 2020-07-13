e4a09f84bb6c7d6b4a2bcfddaefa2a12a2808a6a
Kyle Williams <kwilliams@peakactivity.com>
2020-07-13 18:47:26 -0400

    this is really from last week :/

--- client/src/Login.js
+++ client/src/Login.js
@@ -1,8 +1,66 @@
 import * as React from 'react'
+import { useHistory } from "react-router-dom"
 
+// localStorage['usersTable'] = [{username: "grammerKid", password: "abc123"}]
 
 const Login = () => {
-    return <h1>LOGIN</h1>
+    const [usersTable, setUsersTable] = React.useState(
+        // WE HAVE TO JSON.PARSE BC WE STRINGIFIED IT
+        JSON.parse(localStorage.getItem('usersTable')) || []
+    ) // <== database
+    const [newUser, setNewUser] = React.useState({});
+    const windowActive = typeof window !== 'undefined'
+    const thisHistory = useHistory()
+    
+    const signIn = () => {
+        if (windowActive) {
+            const signedIn = window.localStorage.setItem('isSignedIn', true)
+            console.log('SIGNING IN RESPONSE', signedIn)
+        }
+    }
+
+    const submitHandler = ()=>{
+        // validation (SKIPPING THIS FOR NOW)
+
+        // if exists ✓
+        const userExists = usersTable.find(user => user.username === newUser.username)
+        if(userExists){
+            signIn()
+            thisHistory.push('/dashboard')
+        } else {
+            thisHistory.push('/signup')
+        }
+        // add to table ✓
+
+        // redirect to login page ✓
+        
+        
+    }
+
+    const updateUsername = (e) => {
+        console.log('THE INSERTED USERNAME IS', e.target.value)
+        setNewUser({ ...newUser, username: e.target.value })
+    }
+
+    const updatePassword = (e) => {
+        console.log('THE INSERTED PASSWORD IS ', e.target.value)
+        setNewUser({ ...newUser, password: e.target.value })
+        
+    }
+    return (
+        <React.Fragment>
+            <h1>Login</h1>
+            {/* <button onClick={signIn}>SIGN THIS PUPPY IN</button> */}
+            <form>
+                <label htmlFor="userName">userName</label>
+                <input id="userName" placeholder="username" value={newUser.username} onChange={updateUsername} />
+                <label htmlFor="password">password</label>
+                <input id="password" placeholder="password" value={newUser.password} onChange={updatePassword} />
+                <button id="submit" onClick={submitHandler}>Submit</button>
+            </form>
+            
+        </React.Fragment>
+    )
 }
 
-export default Login
+export default Login;
--- client/src/SignUp.js
+++ client/src/SignUp.js
@@ -1,7 +1,16 @@
 import * as React from 'react'
+import { useHistory } from "react-router-dom"
+
+// localStorage['usersTable'] = [{username: "grammerKid", password: "abc123"}]
 
 const SignUp = () => {
+    const [usersTable, setUsersTable] = React.useState(
+        // WE HAVE TO JSON.PARSE BC WE STRINGIFIED IT
+        JSON.parse(localStorage.getItem('usersTable')) || []
+    ) // <== database
+    const [newUser, setNewUser] = React.useState({});
     const windowActive = typeof window !== 'undefined'
+    const thisHistory = useHistory()
     
     const signIn = () => {
         if (windowActive) {
@@ -9,10 +18,55 @@ const SignUp = () => {
             console.log('SIGNING IN RESPONSE', signedIn)
         }
     }
+
+    const submitHandler = ()=>{
+        // validation (SKIPPING THIS FOR NOW)
+
+        // if exists ✓
+        const userExists = usersTable.find(user => user.username === newUser.username)
+        if(userExists){
+            alert('YOU HAVE ALREADY SIGNED UP NUMNUTS')
+            console.warn('%cYOU HAVE ALREADY SIGNED UP NUMNUTS', 'color: red, background-color: black')
+        } else {
+            const newData = JSON.stringify([
+                ...usersTable,
+                newUser
+            ])
+            console.log('NEW DATA', newData)
+            localStorage.setItem('usersTable', newData)
+
+            signIn()
+            thisHistory.push('/login')
+        }
+        // add to table ✓
+
+        // redirect to login page ✓
+        
+        
+    }
+
+    const updateUsername = (e) => {
+        console.log('THE INSERTED USERNAME IS', e.target.value)
+        setNewUser({ ...newUser, username: e.target.value })
+    }
+
+    const updatePassword = (e) => {
+        console.log('THE INSERTED PASSWORD IS ', e.target.value)
+        setNewUser({ ...newUser, password: e.target.value })
+        
+    }
     return (
         <React.Fragment>
             <h1>Sign Up</h1>
-            <button onClick={signIn}>SIGN THIS PUPPY IN</button>
+            {/* <button onClick={signIn}>SIGN THIS PUPPY IN</button> */}
+            <form>
+                <label htmlFor="userName">userName</label>
+                <input id="userName" placeholder="username" value={newUser.username} onChange={updateUsername} />
+                <label htmlFor="password">password</label>
+                <input id="password" placeholder="password" value={newUser.password} onChange={updatePassword} />
+                <button id="submit" onClick={submitHandler}>Submit</button>
+            </form>
+            
         </React.Fragment>
     )
 }
